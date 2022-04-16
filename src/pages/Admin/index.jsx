a2869294a2c70b9e299259a36efa21800061293f
bao longhui <547525253@qq.com>
2022-04-16 16:21:15 +0800

    懒加载包添加魔法注释，标识包名。 剔除无用代码

--- src/pages/Admin/index.jsx
+++ src/pages/Admin/index.jsx
@@ -2,100 +2,22 @@ import React, { Suspense, lazy } from 'react'
 import { Route, Redirect, Switch } from 'react-router-dom'
 import Layout from '@/components/Layout'
 import ROUTES from '@/constants/routes'
-// import { _getArtilces } from '@/network/article'
-// import { ACTIONS as ARTICLE_ACTIONS } from '@/redux/reducers/articlesState'
-// import { _getAllDrafts } from '@/network/drafts'
-// import { ACTIONS as DRAFT_ACTIONS } from '@/redux/reducers/draftsState'
-// import { _getAllTag } from '@/network/tag'
-// import { ACTIONS as TAG_ACTIONS } from '@/redux/reducers/tagState'
-// import { _getClassifies } from '@/network/classify'
-// import { ACTIONS as CLASSIFY_ACTIONS } from '@/redux/reducers/classifyState'
-// import { _getsays } from '@/network/say'
-// import { ACTIONS as SAY_ACTIONS } from '@/redux/reducers/sayState'
-// import { _getFriendLinks } from '@/network/friendLink'
-// import { ACTIONS as LINK_ACTIONS } from '@/redux/reducers/linkState'
-// import { _getmsgs } from '@/network/msg'
-// import { ACTIONS as MSG_ACTIONS } from '@/redux/reducers/msgState'
-// import { useDispatch } from 'react-redux'
 
-const Home = lazy(() => import('./Home'))
-const Articles = lazy(() => import('./Articles'))
-const AddArticle = lazy(() => import('./AddArticle'))
-const Gallery = lazy(() => import('./Gallery'))
-const AddGallery = lazy(() => import('./AddGallery'))
-const Say = lazy(() => import('./Say'))
-const Msg = lazy(() => import('./Msg'))
-const FriendLink = lazy(() => import('./FriendLink'))
-const Works = lazy(() => import('./Works'))
-const About = lazy(() => import('./About'))
-const AboutEdit = lazy(() => import('./AboutEdit'))
-const WebSiteLogs = lazy(() => import('./WebSiteLogs'))
-const Drafts = lazy(() => import('./Drafts'))
+const Home = lazy(() => import(/* webpackChunkName: "home" */'./Home'))
+const Articles = lazy(() => import(/* webpackChunkName: "articles" */'./Articles'))
+const AddArticle = lazy(() => import(/* webpackChunkName: "addArticle" */'./AddArticle'))
+const Gallery = lazy(() => import(/* webpackChunkName: "gallery" */'./Gallery'))
+const AddGallery = lazy(() => import(/* webpackChunkName: "addGallery" */'./AddGallery'))
+const Say = lazy(() => import(/* webpackChunkName: "say" */'./Say'))
+const Msg = lazy(() => import(/* webpackChunkName: "msg" */'./Msg'))
+const FriendLink = lazy(() => import(/* webpackChunkName: "friendLink" */'./FriendLink'))
+const Works = lazy(() => import(/* webpackChunkName: "works" */'./Works'))
+const About = lazy(() => import(/* webpackChunkName: "about" */'./About'))
+const AboutEdit = lazy(() => import(/* webpackChunkName: "aboutEdit" */'./AboutEdit'))
+const WebSiteLogs = lazy(() => import(/* webpackChunkName: "webSiteLogs" */'./WebSiteLogs'))
+const Drafts = lazy(() => import(/* webpackChunkName: "drafts" */'./Drafts'))
 
 const Admin = () => {
-  // const dispatch = useDispatch()
-  // const getArticlesFromDb = async () => {
-  //   const articles = await _getArtilces()
-  //   dispatch({
-  //     type: ARTICLE_ACTIONS.GET_ARTICLES,
-  //     payload: articles,
-  //   })
-  // }
-  // const getDraftsFromDb = async () => {
-  //   const drafts = await _getAllDrafts()
-  //   dispatch({
-  //     type: DRAFT_ACTIONS.GET_DRAFTS,
-  //     payload: drafts,
-  //   })
-  // }
-  // const getTagsFromDb = async () => {
-  //   const tags = await _getAllTag()
-  //   dispatch({
-  //     type: TAG_ACTIONS.GET_TAGS,
-  //     payload: tags,
-  //   })
-  // }
-  // const getClassifiesFromDb = async () => {
-  //   const classifies = await _getClassifies()
-  //   dispatch({
-  //     type: CLASSIFY_ACTIONS.GET_CLASSIFIES,
-  //     payload: classifies,
-  //   })
-  // }
-
-  // const getSaysFromDb = async () => {
-  //   const says = await _getsays()
-  //   dispatch({
-  //     type: SAY_ACTIONS.GET_SAYS,
-  //     payload: says,
-  //   })
-  // }
-
-  // const getFriendLinksFromDb = async () => {
-  //   const friendLinks = await _getFriendLinks()
-  //   dispatch({
-  //     type: LINK_ACTIONS.GET_FRIEND_LINKS,
-  //     payload: friendLinks,
-  //   })
-  // }
-
-  // const getMsgsFromDb = async () => {
-  //   const msgs = await _getmsgs()
-  //   dispatch({
-  //     type: MSG_ACTIONS.GET_MSGS,
-  //     payload: msgs,
-  //   })
-  // }
-
-  // useEffect(() => {
-  //   getArticlesFromDb()
-  //   getDraftsFromDb()
-  //   getTagsFromDb()
-  //   getClassifiesFromDb()
-  //   getSaysFromDb()
-  //   getFriendLinksFromDb()
-  //   getMsgsFromDb()
-  // }, [])
 
   return (
     <Layout>
